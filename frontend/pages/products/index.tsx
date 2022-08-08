b11293e94c6adf860475ceaefbb4ef9e73baa38d
DESKTOP-B7BQRKK\alex1 <alex1151212@gmail.com>
2022-08-08 22:21:37 +0800

    Shallow routing

--- frontend/pages/products/index.tsx
+++ frontend/pages/products/index.tsx
@@ -1,17 +1,41 @@
-import { getAllProduct } from "../../fake-data";
+import { sortByPrice, Direction } from "../../fake-data";
 import ProductCard from "../../components/ProductCard";
-import { PageTitle, ProductGallery } from "./index.style";
-
+import { PageTitle, ProductGallery, PriceFilter } from "./index.style";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 const Home = () => {
-  const products = getAllProduct();
+  const [direction, setDirection] = useState<Direction>("ASC");
+  const products = sortByPrice(direction);
+  const router = useRouter();
+
+  const handleSortingDirectionChange = (
+    e: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    const dir = e.target.value;
+    router.push(`${router.pathname}?direction=${dir}`, undefined, {
+      shallow: true,
+    });
+  };
+  useEffect(() => {
+    if (router.query.direction) {
+      setDirection(router.query.direction as Direction);
+    }
+  }, [router.query.direction]);
 
   return (
     <>
       <PageTitle>商品列表</PageTitle>
+      <PriceFilter>
+        Price:
+        <select value={direction} onChange={handleSortingDirectionChange}>
+          <option value="ASC">價格由低到高</option>
+          <option value="DES">價格由高到低</option>
+        </select>
+      </PriceFilter>
       <ProductGallery>
-        {products.map((product) => {
-          return <ProductCard key={product.id} product={product}></ProductCard>;
-        })}
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
       </ProductGallery>
     </>
   );
